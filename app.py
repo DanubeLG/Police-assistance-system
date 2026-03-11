bdd6da551d6aabc750ed099296090b5660be4f98
Pranav-Ajay <pranavajay005@gmail.com>
2026-03-11 10:38:01 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,51 +1,50 @@
-#code to connect back to mysql just in case postgre is not viable
-'''
-import mysql.connector
-return mysql.connector.connect(
-        host="localhost",
-        port=3306,
-        user="root",
-        password="0007",
-        database="miniproject")
-'''
 import psycopg2
+from psycopg2.extras import RealDictCursor
 
-from flask import Flask, request, jsonify
-from flask import render_template
+from flask import Flask, request, jsonify, render_template
 from flask_cors import CORS
 
 app = Flask(__name__)
 CORS(app)
 
 def get_db():
-    db_url=r"postgresql://root:gpJyxnlfoUCX9RorIolq0khn7phiJerT@dpg-d6nqo49aae7s738jok60-a/miniproject_dp02"
-    conn=psycopg2.connect(db_url)
+    db_url = "postgresql://root:gpJyxnlfoUCX9RorIolq0khn7phiJerT@dpg-d6nqo49aae7s738jok60-a/miniproject_dp02"
+    conn = psycopg2.connect(db_url)
     return conn
+
 
 @app.route("/users", methods=["GET"])
 def get_users():
+
     db = get_db()
-    cursor = db.cursor(dictionary=True)
+    cursor = db.cursor(cursor_factory=RealDictCursor)
+
     cursor.execute("SELECT uid, post, password FROM users ORDER BY post")
+
     data = cursor.fetchall()
+
     cursor.close()
     db.close()
+
     return jsonify(data)
+
 
 @app.route("/login", methods=["POST"])
 def login():
+
     data = request.get_json()
 
     uid = data.get("uid")
     password = data.get("password")
 
     db = get_db()
-    cursor = db.cursor(dictionary=True)
+    cursor = db.cursor(cursor_factory=RealDictCursor)
 
     cursor.execute(
         "SELECT * FROM users WHERE uid=%s AND password=%s",
         (uid, password)
     )
+
     user = cursor.fetchone()
 
     cursor.close()
@@ -56,13 +55,16 @@ def login():
     else:
         return jsonify({"status": "fail"})
 
+
 @app.route("/")
 def home():
     return render_template("index.html")
 
+
 @app.route("/profile1")
 def check():
     return render_template("profile1.html")
+
 
 @app.route("/chatbot")
 def check1():
@@ -71,6 +73,3 @@ def check1():
 
 if __name__ == "__main__":
     app.run(debug=True)
-
-
-
