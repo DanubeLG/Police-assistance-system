6b2e601e7be245bbda62575e3987183570362488
DanubeLG <u2308020@rajagiri.edu.in>
2026-03-11 23:33:27 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 import psycopg2
-#from psycopg2.extras import RealDictCursor
-#cursor_factory=RealDictCursor
-from flask import Flask, request, jsonify, render_template
+
+from flask import Flask, request, jsonify
 from flask import render_template
 from flask_cors import CORS
 
@@ -9,55 +8,51 @@ app = Flask(__name__)
 CORS(app)
 
 def get_db():
-    db_url =r"postgresql://root:gpJyxnlfoUCX9RorIolq0khn7phiJerT@dpg-d6nqo49aae7s738jok60-a/miniproject_dp02"
-    conn = psycopg2.connect(db_url)
+    db_url=r"postgresql://root:gpJyxnlfoUCX9RorIolq0khn7phiJerT@dpg-d6nqo49aae7s738jok60-a/miniproject_dp02"
+    conn=psycopg2.connect(db_url)
     return conn
-
 
 @app.route("/users", methods=["GET"])
 def get_users():
+    db = get_db()
+    cursor = db.cursor(dictionary=True)
+    cursor.execute("SELECT uid, post, password FROM users ORDER BY post")
+    data = cursor.fetchall()
+    cursor.close()
+    db.close()
+    return jsonify(data)
+
+@app.route("/login", methods=["POST"])
+def login():
+    data = request.get_json()
+
+    uid = data.get("uid")
+    password = data.get("password")
 
     db = get_db()
-    cursor = db.cursor()
+    cursor = db.cursor(dictionary=True)
 
-    cursor.execute("SELECT uid, post, password FROM users ORDER BY post")
-
-    data = cursor.fetchall()
+    cursor.execute(
+        "SELECT * FROM users WHERE uid=%s AND password=%s",
+        (uid, password)
+    )
+    user = cursor.fetchone()
 
     cursor.close()
     db.close()
 
-    return jsonify(data)
-
-
-@app.route("/login", methods=["POST"])
-def login():
-    try:
-        data = request.get_json()
-        uid = data.get("uid")
-        password = data.get("password")
-        db = get_db()
-        cursor = db.cursor()
-        cursor.execute("SELECT * FROM users WHERE uid=%s AND password=%s",(uid, password))
-        user = cursor.fetchone()
-        cursor.close()
-        db.close()
-        if user:
-            return jsonify({"status": "success"})
-        else:
-            return jsonify({"status": "fail"})
-    except Exception as e:
-        return jsonify({"error": str(e)})
+    if user:
+        return jsonify({"status": "success"})
+    else:
+        return jsonify({"status": "fail"})
 
 @app.route("/")
 def home():
     return render_template("index.html")
 
-
 @app.route("/profile1")
 def check():
     return render_template("profile1.html")
-
 
 @app.route("/chatbot1")
 def check1():
@@ -66,3 +61,5 @@ def check1():
 
 if __name__ == "__main__":
     app.run(debug=True)
+
+
